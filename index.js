1e94f27cef1c72f2ef1eef9faba6d99f24a4476c
isooplik <idbbs@g.hongik.ac.kr>
2025-10-23 11:55:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -21,20 +21,27 @@ const qs = require("querystring");
     });
     const message = (_a = core.getInput("message")) !== null && _a !== void 0 ? _a : "티켓사세요";
     const webhook = new webhook_1.IncomingWebhook(webhookUrl);
-    const res = await axios_1.default({
-        method: "POST",
-        url: "https://ticket.melon.com/tktapi/product/seatStateInfo.json",
-        params: {
-            v: "1",
-        },
-        data: qs.stringify({
-            prodId: productId,
-            scheduleNo: scheduleId,
-            seatId,
-            volume: 1,
-            selectedGradeVolume: 1,
-        }),
-    });
+    const res = await axios.default({
+  method: "POST",
+  url: "https://ticket.melon.com/ktapp/product/seatStateInfo.json",
+  headers: {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+    "Referer": "https://ticket.melon.com/performance/index.htm?prodId=212148",
+    "Origin": "https://ticket.melon.com",
+    "Cookie": PCID=17589803545352185526445; _fwb=201xuXSYVc9LrnBUGzShdxX.1758980354561; PC_PCID=17589803545352185526445; TKT_POC_ID=MP15; MAC=dl9EPgizAdRpiOnYNTTRkCjDIsivfXRgNZcvq3KWv1hqW2V12kz9xv5OPF5ctDBD; MLCP=NDI5NTQyNzYlM0JzeWJhbmc0OTc2MSUzQiUzQjAlM0JleUpoYkdjaU9pSklVekkxTmlKOS5leUpwYzNNaU9pSnRaVzFpWlhJdWJXVnNiMjR1WTI5dElpd2ljM1ZpSWpvaWJXVnNiMjR0YW5kMElpd2lhV0YwSWpveE56WXhNVGcwTXpnMkxDSnRaVzFpWlhKTFpYa2lPaUkwTWprMU5ESTNOaUlzSW1GMWRHOU1iMmRwYmxsT0lqb2lUaUo5Lmpzbi1pem5IalhqOGJIeUtTMWlpeldmZWJhanAyRmJJWjVQR2pDRTRDaHclM0IlM0IyMDI1MTAyMzEwNTMwNiUzQiVFQiVCMCVBOSVFQSVCRSVCOSVFQiVCRCU5NSUzQjElM0JzeWJhbmc0OTc2JTQwbmF2ZXIuY29tJTNCMyUzQg==; MUS=176532442; keyCookie=42954276; store_melon_cupn_check=42954276; NetFunnel_ID=WP15; JSESSIONID=91810FC57AA8CD8F889ADB27C3F556D3; wcs_bt=s_585b06516861:1761187890
+  },
+  params: {
+    v: "1",
+  },
+  data: qs.stringify({
+    productId,
+    scheduleId,
+    seatId,
+    selectedGradeVolume: 1,
+    volume: 1,
+  }),
+});
+
     // tslint:disable-next-line
     console.log("Got response: ", res.data);
     if (res.data.chkResult) {
